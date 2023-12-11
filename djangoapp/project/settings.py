a743a11ca1a06ece561ceb50e3cbfa91f472cf4a
Castelli <isisdare@gmail.com>
2023-12-11 07:08:24 -0300

    Configurando o Dockerfile para gerar nossa imagem Docker Django

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+# flake8: noqa
+
 import os
 from pathlib import Path
 
