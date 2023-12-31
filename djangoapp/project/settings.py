b518dd8f4af1756910011cb86ca08f9cfa3ebad1
Castelli <isisdare@gmail.com>
2023-12-30 22:41:49 -0300

    Criando o app blog

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,7 @@ STATIC_ROOT = DATA_DIR / 'static'
 MEDIA_URL = '/media/'
 # /data/web/media
 MEDIA_ROOT = DATA_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
